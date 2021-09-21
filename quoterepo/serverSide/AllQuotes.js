a723e47f74db122d0f08dab399bf74235f328278
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2021-09-21 15:17:40 -0400

    hi

--- quoterepo/serverSide/AllQuotes.js
+++ quoterepo/serverSide/AllQuotes.js
@@ -3,39 +3,38 @@
 // Then search using findQuote
 //   - N. Wilde, July 2020
 class AllQuotes {
-    quoteArray = [];
+	quoteArray = [];
 
-    addQuote(str) {
-        this.quoteArray.push(str);
-    } // end addQuote
+	addQuote(str) {
+		this.quoteArray.push(str);
+	} // end addQuote
 
-    loadFromFile(thePath){
-        // assume file has fields separated by ";"
-        // the first field has the quote
-        let fs = require('fs');
-        let contents = fs.readFileSync(thePath, "utf8");
-        let records = contents.split("\n");
-        let numRecords = records.length;
-        for(let j = 0; j < numRecords; j++){
-            let aLine = records[j];
-            // need to exclude the empty record produced by split
-            if( aLine.length > 0) {
-                let fields = aLine.split(";");
-                this.addQuote(fields[0]);
-            }
-        }
-    } // end loadFromFile
+	loadFromFile(thePath) {
+		// assume file has fields separated by ";"
+		// the first field has the quote
+		let fs = require("fs");
+		let contents = fs.readFileSync(thePath, "utf8");
+		let records = contents.split("\n");
+		let numRecords = records.length;
+		for (let j = 0; j < numRecords; j++) {
+			let aLine = records[j];
+			// need to exclude the empty record produced by split
+			if (aLine.length > 0) {
+				let fields = aLine.split(";");
+				this.addQuote(fields[0]);
+			}
+		}
+	} // end loadFromFile
 
-    findQuote(qString){
-        let result = "No matching quote was found";
-        this.quoteArray.forEach( (q) => {
-            if (q.search(qString) > -1 ){
-                // found a matching quote
-                result = q;
-            }
-        } );
-        return result;
-    } // end findQuote()
-
+	findQuote(qString) {
+		let result = "No matching quote was found";
+		this.quoteArray.forEach((q) => {
+			if (q.search(qString) > -1) {
+				// found a matching quote
+				result = q;
+			}
+		});
+		return result;
+	} // end findQuote()
 } // end class AllQuotes
 module.exports = AllQuotes;
