d784f7e94e074f361cd34c4adbfab69747efcec6
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2021-10-15 15:34:46 -0400

    standard commit message

---
+++ quoterepo/serverSide/start.sh
@@ -0,0 +1,9 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+sudo aws s3 cp "s3://asmit-work-bucket/deployment.tar.gz" deployment.tar.gz
+sudo gunzip deployment.tar.gz
+sudo tar -xf deployment.tar
+cd serverSide
+npm install pm2@latest -g
+pm2 start server.js
