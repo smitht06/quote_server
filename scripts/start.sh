0f116edaf7fe5a45f33f73aafb9b288aefe16c1f
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2021-10-18 16:32:56 -0400

    standard commit message

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
-\. /home/ec2-user/.bashrc
+source /home/ec2-user/.bash_profile
 
 pm2 kill
 pm2 stop
-pm2 start -f /home/ec2-user/quoterepo/serverSide/server.js
+pm2 start /home/ec2-user/quoterepo/serverSide/server.js
