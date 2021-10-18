13fdcb9530e8b7b5fa80650f6f09279513d97f64
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2021-10-18 10:23:48 -0400

    standard commit message

--- quoterepo/serverSide/start.sh
+++ quoterepo/serverSide/start.sh
@@ -1,10 +1,11 @@
 source /home/ec2-user/.bash_profile
+sudo su ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 sudo aws s3 cp "s3://asmit-work-bucket/deployment.tar.gz" deployment.tar.gz
 sudo gunzip deployment.tar.gz
 sudo tar -xf deployment.tar
-cd serverSide
+cd /serverSide
 npm install pm2@latest -g
 pm2 start server.js
